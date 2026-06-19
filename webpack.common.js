8add28bd28e7179c8c1b540d86b0bb1cd8460247
sahil-dev <232035734+sahilCodes-01@users.noreply.github.com>
2026-06-19 07:42:40 +0000

    refactor: split webpack config into common/dev/prod with webpack-merge

--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,11 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
-  },
-
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"],
   },
 
   plugins: [
